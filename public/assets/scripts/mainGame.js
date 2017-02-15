bbc7c21a4bd5786ce3d5f05240553293ba9b32fe
Louis Antweiler <Louis.Antweiler@Gmail.com>
2017-02-15 13:28:20 -0700

    add player animations

--- public/assets/scripts/mainGame.js
+++ public/assets/scripts/mainGame.js
@@ -1,19 +1,134 @@
 var mainGame = function(game) {
 }
 
+var wizard;
+var cursors;
+var characterVelocity = 200;
+var characterFrameRate = 16;
+var game;
+var worldGravity = 250;
+var facing = 'right';
+
 mainGame.prototype = {
   create: function() {
-    var wizard = this.game.add.sprite(0, 0, "wizard")
+    game = this.game;
+
+    wizard = game.add.sprite(50, 50, "wizard")
     wizard.animations.add('walkRight', [70, 71, 72, 73])
-    wizard.animations.play('walkRight', 16, true)
-    var wizard2 = this.game.add.sprite(100, 100, "wizard")
-    wizard2.animations.add('elecJump', [39, 40, 41, 42, 43, 44, 45, 46, 47])
-    wizard2.animations.play('elecJump', 16, true)
-    var wizard3 = this.game.add.sprite(200, 200, "wizard")
-    wizard3.animations.add('iceGround', [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27])
-    wizard3.animations.play('iceGround', 16, true)
-    var wizard4 = this.game.add.sprite(300, 300, "wizard")
-    wizard4.animations.add('jump', [78, 79, 80, 81])
-    wizard4.animations.play('jump', 16, true)
+    wizard.animations.add('walkLeft', [66, 67, 68, 69])
+    wizard.animations.add('elecJumpRight', [38, 39, 40, 41, 42, 43, 44, 45, 46, 47])
+    wizard.animations.add('elecJumpLeft', [28, 29, 30, 31, 32, 33, 34, 35, 36, 37])
+    wizard.animations.add('iceGroundRight', [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 15, 14])
+    wizard.animations.add('iceGroundLeft', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 0])
+    wizard.animations.add('jumpRight', [78, 79, 80, 81])
+    wizard.animations.add('jumpLeft', [74, 75, 76, 77])
+
+    game.physics.enable(wizard, Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = worldGravity;
+
+    wizard.body.collideWorldBounds = true;
+    // wizard.body.setSize(20, 32, 5, 16);
+
+
+    cursors = game.input.keyboard.createCursorKeys();
+    spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+    
+    spacebar.onDown.add(this.jump)
+    cursors.down.onDown.add(this.iceGround);
+  },
+  update: function() {
+    // console.log(wizard.body.y)
+    // wizard.animations.stop();
+    if (wizard.body.y === 400) {
+      if (cursors.right.isDown) {
+        facing = 'right'
+        wizard.body.velocity.x = characterVelocity;
+        wizard.animations.play("walkRight", characterFrameRate, true)
+      } else if (cursors.left.isDown) {
+        facing = 'left'
+        wizard.body.velocity.x = -characterVelocity;
+        wizard.animations.play("walkLeft", characterFrameRate, true)
+      } else {
+        if (facing !== "iceGroundLeft" && facing !== "iceGroundRight") {
+          wizard.animations.stop();
+          wizard.body.velocity.x = 0;
+          if (facing === 'right') {
+            wizard.frame = 81;
+          } else {
+            wizard.frame = 77;
+          }
+        }
+      }
+    } else {
+      if (facing !== "electricRight" && facing !== "electricLeft") {
+        if (facing === "right") {
+          wizard.frame = 80;
+        } else if (facing === "left") {
+          wizard.frame = 76;
+        }
+        if (cursors.right.isDown) {
+          facing = 'right'
+          wizard.body.velocity.x = characterVelocity;
+        } else if (cursors.left.isDown) {
+          facing = 'left'
+          wizard.body.velocity.x = -characterVelocity;
+        }
+      } else {
+        if (cursors.right.isDown) {
+          wizard.body.velocity.x = characterVelocity;
+        } else if (cursors.left.isDown) {
+          wizard.body.velocity.x = -characterVelocity;
+        }
+      }
+
+    }
+
+  },
+
+  jump: function() {
+
+
+    if (wizard.body.y === 400) {
+      if (facing === 'left') {
+        wizard.animations.play("jumpLeft", characterFrameRate, false)
+
+      } else if (facing === 'right') {
+        wizard.animations.play("jumpRight", characterFrameRate, false)
+      }
+      wizard.body.velocity.y = -200;
+    } else {
+      if (facing === 'left') {
+        facing = "electricLeft"
+        wizard.animations.play("elecJumpLeft", characterFrameRate, false)
+
+
+      } else if (facing === 'right') {
+        facing = "electricRight"
+        wizard.animations.play("elecJumpRight", characterFrameRate, false)
+      }
+    }
+  },
+
+  iceGround: function() {
+    if (wizard.body.y === 400) {
+      if (facing === 'left') {
+        facing = "iceGroundLeft"
+        wizard.body.velocity.x = 0;
+        wizard.animations.play("iceGroundLeft", characterFrameRate, false)
+        wizard.animations.currentAnim.onComplete.add(function () {
+          facing = "left";
+        }, this);
+
+      } else if (facing === 'right') {
+        facing = "iceGroundRight"
+        wizard.body.velocity.x = 0;
+        wizard.animations.play("iceGroundRight", characterFrameRate, false)
+        wizard.animations.currentAnim.onComplete.add(function () {
+          facing = "right";
+        }, this);
+
+      }
+    }
+
   }
 }
