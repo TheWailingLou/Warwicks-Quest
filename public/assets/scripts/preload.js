a0cc253438ddeda66ef9eb915ebf8eede785b4ae
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-02-17 11:33:15 -0700

    skeleton and mummy pat

--- public/assets/scripts/preload.js
+++ public/assets/scripts/preload.js
@@ -5,6 +5,8 @@ preload.prototype = {
   preload: function() {
     this.game.load.atlasJSONHash('wizard', 'assets/sprites/wizardGuy.png', 'assets/sprites/wizardGuy.json');
     this.game.load.atlasJSONHash('monster', 'assets/sprites/msMonster.png', 'assets/sprites/msMonster.json');
+    this.game.load.atlasJSONHash('skelly', 'assets/sprites/skelly.png', 'assets/sprites/skelly.json');
+    this.game.load.atlasJSONHash('mummy', 'assets/sprites/msMummy.png', 'assets/sprites/msMummy.json');
     this.game.load.image('mainTiles', 'assets/world/tilesets/dawnOfGods.png');
     this.game.load.tilemap('testLevel', 'assets/world/CSV/Test.csv', null, Phaser.Tilemap.CSV);
     this.game.load.tilemap('testBack', 'assets/world/CSV/LayerTest2_TileLayer1.csv', null, Phaser.Tilemap.CSV);
