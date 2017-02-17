9bc2ede5c6b589dccbb9e2b59ad950d453ef2ed3
Tom Krepp <tommykrepp@aol.com>
2017-02-16 19:20:17 -0700

    Menu-Start

--- public/assets/scripts/preload.js
+++ public/assets/scripts/preload.js
@@ -7,8 +7,10 @@ preload.prototype = {
     this.game.load.atlasJSONHash('monster', 'assets/sprites/msMonster.png', 'assets/sprites/msMonster.json');
     this.game.load.image('mainTiles', 'assets/world/tilesets/dawnOfGods.png');
     this.game.load.tilemap('testLevel', 'assets/world/CSV/Test.csv', null, Phaser.Tilemap.CSV);
+    this.game.load.spritesheet('button-start', 'assets/images/button-start.png', 401, 143);
+    this.game.load.image('button-pause', 'assets/images/button-pause.png');
   },
   create: function() {
-    this.game.state.start("mainGame")
+    this.game.state.start("mainMenu")
   }
 }
